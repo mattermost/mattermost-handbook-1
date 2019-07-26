ebb5208a892e20d6f06e28f09e819ba15a5e4bc4
Camille Harris <32655234+comharris@users.noreply.github.com>
2019-07-25 23:26:26 -0700

    Update and rename staff-onboarding-guide.md to general-onboarding-guide.r

--- source/people/general-onboarding-guide.r
+++ source/people/general-onboarding-guide.r
@@ -1,22 +1,22 @@
-# WIP: Staff Onboarding Guide
+WIP: Staff Onboarding Guide
+====================
 
-### Welcome to Mattermost
-------------------------------------------
+Welcome to Mattermost
+----------------------------
 
 We’re on a mission to make the world safer and more productive through open source. To make that happen we first have to create a great place to work--an inclusive, courteous online community where incredibly talented individuals are empowered to do their best work for our customers, with very little in their way. This guide is a summary of our guiding principles. As Mattermost grows, we hope these principles serve each new staff person that joins us. If you are new to Mattermost, welcome! While the goals here are important, it’s your ideas, talent, and energy that will keep Mattermost shining in the years ahead. Thanks for being here. Let’s change the world.
 
-### How to use our Handbook
-------------------------------------------
+How to use our Handbook
+----------------------------
 
 Our handbook defines what it’s like to work at Mattermost, from the high level to specific operating procedures. If you’re new, you’ll start with our onboarding guides.
 
-### Our Mission
-------------------------------------------
-
+Our Mission
+----------------------------
 Our mission is to make the world safer and more productive by developing and delivering secure, open source collaboration software that is trusted, flexible and offers fast time-to-value.
 
-### Leadership Principles 
-------------------------------------------
+Leadership Principles 
+----------------------------
 
 Leadership principles are deliberate choices defining our behavior. When facing complexity, uncertainty, or ambiguity (CUA) we determine our point of view and our actions through the lens of our values:
 
@@ -32,8 +32,8 @@ Leadership principles are deliberate choices defining our behavior. When facing 
 
 - **Earn Trust** — Make decisions based on maximizing the trust of others in your judgment. Be open, self-critical and factual.
 
-### Products
-------------------------------------------
+Products
+----------------------------
 
 Mattermost Team Edition under MIT license and the Mattermost open source projects under various licenses are maintained by core committers, including both staff from Mattermost, Inc. and volunteer contributors from the Mattermost user and customer communities.
 
@@ -41,8 +41,8 @@ Mattermost Enterprise Editions, which are commercial extensions built on top of 
 
 For more information, see our [Product Overview](https://docs.mattermost.com/overview/product.html).
 
-### Company 
--------------- 
+Company 
+---------------------------- 
 
 Mattermost is an open source, remote-first, communities-centered company based in Palo Alto, California and headquartered on the internet. 
 
@@ -56,39 +56,42 @@ Remote-first culture flourishes when we share one simple principle: **Courtesy**
 
 **Based in Palo Alto, California and headquartered on the internet** means the mailing address for Mattermost, Inc. is in Palo Alto, California, and our headquarters is on the internet, specifically the Mattermost instance at https://community.mattermost.com. Our online headquarters is where Mattermost staff work with our communities of colleagues, users, partners, customers and contributors, to envision, develop and refine new open source technologies to make the world safer and more productive. 
 
-## General Onboarding
+General Onboarding
+----------------------------
 
-### Before Starting at Mattermost
-------------------------------------------
-
-#### T-minus 3 weeks
+T-minus 3 weeks
+----------------------------
  
 1. (People Ops) Sends an [email](https://docs.google.com/document/d/1TX2pnJebl7Mi2-R5u3R6PsjX8YOMS54xcI0KJhh9_xI/edit#bookmark=id.srysr7dn6fzd) to find out New Hire's preference for laptop and equipment, either to be purchased or taken from stock and shipped by People Ops or purchased locally by New Hire and expensed.
 2. (People Ops) Sends the [Mattermost email agreement](https://docs.google.com/document/d/1PhkQkvoaunu8V8qjtmt6GmZoIMZI8sq01C1nG-FoHQo/edit?usp=sharing) via DocuSign before issuing New Hire an @mattermost.com email address. New Hire should use this email address on community.mattermost.com (replace personal email with company email if already registered there). FIRST_NAME.LAST_NAME@mattermost.com is the standard naming convention.
 3. (People Ops) Sends New Hire a welcome package containing Mattermost branded goodies.
 
-#### T-minus 2 weeks
+T-minus 2 weeks
+----------------------------
 
 4. (Head of HR) Sends New Hire an [email](https://docs.google.com/document/d/1TX2pnJebl7Mi2-R5u3R6PsjX8YOMS54xcI0KJhh9_xI/edit#bookmark=kix.9dj4d3aa8un9) about payroll and benefits.
 5. (People Ops) Sends New Hire an [email](https://docs.google.com/document/d/1TX2pnJebl7Mi2-R5u3R6PsjX8YOMS54xcI0KJhh9_xI/edit#bookmark=id.tufgijkmrb91) requesting New Hire's biography, inviting New Hire to the Mattermost [demo](https://mattermost.com/demo/) and sharing more about [working at Mattermost](https://docs.mattermost.com/process/working-at-mattermost.html), including our [leadership principles](https://mattermost.com/about-us/).
 
-#### T-minus 1 week
+T-minus 1 week
+----------------------------
 
 6. (People Ops & Hiring Manager) Meet to discuss New Hire's onboarding plan, including tools needed.
 
-#### T-minus 2 days
+T-minus 2 days
+----------------------------
 
 7. (People Ops) Sends New Hire an [email](https://docs.google.com/document/d/1TX2pnJebl7Mi2-R5u3R6PsjX8YOMS54xcI0KJhh9_xI/edit#heading=h.w5heque66i1c) sharing a first day checklist and information about laptop setup, and gives an overview of New Hire's first week.
 8. (People Ops & Hiring Manager) Invite New Hire to [tools used across Mattermost](https://airtable.com/tblI4gu3oPUiZazs8/viwlYaOOIveb3dhLV?blocks=hide), and tools used in their department.
 9. (Hiring Manager) Invites New Hire to departmental and 1:1 meetings.
 10. (People Ops) Invites New Hire to Customer Obsession and Platform meetings.
 
-#### T-minus 1 day
+T-minus 1 day
+----------------------------
 
 11. (New Hire) Sets up laptop and accepts invitations to tools and meetings.
 
-### First Day at Mattermost
-------------------------------------------
+First Day at Mattermost
+----------------------------
 
 1. (People Ops)  Sends New Hire an [email](https://docs.google.com/document/d/1TX2pnJebl7Mi2-R5u3R6PsjX8YOMS54xcI0KJhh9_xI/edit#bookmark=id.tlsyeisvmbc1) answering frequently asked questions, sharing Mattermost's [User's Guide](https://docs.mattermost.com/guides/user.html#getting-started), and suggesting channels the New Hire should join to get a feel for the company. People Ops also sends New Hire a message on Mattermost with the email formatted using Markdown.
 2. (People Ops) Meets with New Hire to make sure they feel welcome, have access to all tools needed, and get an overview of Mattermost's most important features. New Hire is also invited to complete [End User Onboarding](https://academy.mattermost.com/p/end-user-onboarding) training through Mattermost Academy.
@@ -97,69 +100,49 @@ Remote-first culture flourishes when we share one simple principle: **Courtesy**
 5. (Hiring Manager) Meets with New Hire to set expectations for the week, introduce them to their Buddy, share the 30/60/90 plan draft process [TEMPLATE NEEDED] and answer any questions.
 6. (People Ops) Schedules meetings with New Hire (non-R&D only) and Individual Contributors (IC) outside of their department.
 7. (Head of HR) Meets with New Hire to review required documentation (e.g. [I-9 documents](https://www.uscis.gov/i-9)).
-
-
-### Day 2
-------------------------------------------
-
 8. (New Hire) Has a welcome meeting with an IC outside of their department.
 9. (Hiring Manager) Meets with New Hire to check in.
-
-### Day 3
-------------------------------------------
-
 10. (People Ops) Sends New Hire an [email](https://docs.google.com/document/d/1TX2pnJebl7Mi2-R5u3R6PsjX8YOMS54xcI0KJhh9_xI/edit#bookmark=kix.toi80hx08jzs) sharing the [org chart](https://mattermost.bamboohr.com/employees/orgchart.php) and [staff email list](https://docs.google.com/spreadsheets/d/1NQE0fkZgavMTrSSB1aPWg5hGRL182S6AGsa4ts4pWZ4/edit#gid=649832066) and describing how to view other staff members' calendars. 
 11. (New Hire) Has a welcome meeting with an IC outside of their department.
 12. (Hiring Manager) Meets with New Hire to check in.
-
-
-### Day 4
-------------------------------------------
-
 13. (New Hire) Has a welcome meeting with an IC outside of their department.
 14. (Hiring Manager) Meets with New Hire to check in.
-
-
-### Day 5
-------------------------------------------
-
 15. (People Ops) Sends a direct message or [email](https://docs.google.com/document/d/1TX2pnJebl7Mi2-R5u3R6PsjX8YOMS54xcI0KJhh9_xI/edit#bookmark=id.reex8djwhwfa) inviting them to create their Mattermost avatar. New Hire is also asked to review the last three recordings of the [Customer Obsession All Hands Meeting](https://docs.mattermost.com/process/training.html#customer-obsession-all-hands-meeting) and if they would like to present their own intro or if they'd like their manager to introduce them.
 16. (New Hire) New Hire has a welcome meeting with an IC outside of their department.
 17. (Product Manager or Solution Architect) Holds FAQ session on the product.
 18. (Hiring Manager) Meets with New Hire to check in.
 
-### Days 6-10
-------------------------------------------
+Days 6-10
+----------------------------
 
 
 19. (CEO) Meets with New Hire to welcome to Mattermost and discuss their onboarding experience. 
 20. (Hiring Manager) Meets with New Hire to finalize 30/60/90 day plan. Introduces New Hire to entire team during the Customer Obsession meeting.
 21. (Head of HR) (Optional) Meets with New Hire if first day meeting did not occur.
 
-### Days 11-29
-------------------------------------------
-
+Days 11-29
+----------------------------
 22. (Executive Team) Meets with New Hire as a group to share more about themselves, their department and why they came to work at Mattermost.
 
 
-### Day 30
-------------------------------------------
+Day 30
+----------------------------
 
 23. (People Ops) Sends New Hire an [onboarding survey](https://goo.gl/forms/7Ettz9814uiLfxov1) to measure effectiveness of onboarding and improve for future new hires.
 24. (Hiring Manager) Meets with New Hire for a check in meeting to see if they are on track with their 30/60/90 goals.
 
-### Days 50-59
-------------------------------------------
+Days 50-59
+----------------------------
 25. (Hiring Manager) Initiates 360 review [TEMPLATE NEEDED] of New Hire, which is 4-5 questions asked of peers (and direct reports, if applicable) to see how New Hire is doing.
 
 
-### Day 60
-------------------------------------------
+Day 60
+----------------------------
 
 26. (Hiring Manager) Meets with New Hire for a check in meeting and reviews results of 360 review.
 
-### Day 90
-------------------------------------------
+Day 90
+----------------------------
 
 27. (Hiring Manager) Meets with New Hire for a check in meeting and informal performance review [TEMPLATE NEEDED].
 28. (People Ops) Sends New Hire the [Staff Enablement survey](https://docs.google.com/forms/d/e/1FAIpQLSdAtSUG1Sw2AUAajnGrGKg1eHafj9NlINaXcVbgcte9EHWHQg/viewform?usp=sf_link).
